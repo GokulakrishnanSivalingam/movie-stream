8fe1d2f4f281e4fdf909a20c5be2c889a3cb869b
Gokulakrishnan  S <sivalingamgokulakrishnan@gmail.com>
2024-12-22 18:52:36 +0530

    Update Video.jsx

--- src/Video.jsx
+++ src/Video.jsx
@@ -18,9 +18,9 @@ import {useParams} from 'react-router-dom';
         </header>
      <div className="title">   <h1>{video.title}</h1></div>
         <div className="video-container">
-      <video  controls autoPlay  style={{ width: '100%', maxWidth: '95%', height: '100%',
-        maxHeight: '450px', margin: '10px', 
-        borderRadius:'15px',background:'black' }}
+      <video  controls autoPlay  style={{ width: '100%', maxWidth: '100%', height: '100%',
+        maxHeight: '550px', margin: '10px', 
+        borderRadius:'1px',background:'black' }}
          className="video-player">
         <source src={video.videoUrl} type="video/mp4" />
 
